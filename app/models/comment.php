b9c9ff62030d48af089f346b664987ca8109529f
rdaitan <r.daitan@klab.com>
2015-08-25 17:07:32 +0800

    Defined validation rules for Comment model
    Validation rules of a model are defined in its $validation variable.
    This would be used by the validation() method.
    The current rule checks for the length of the 'username' and 'body'
    fields. Length validation uses the validate_between() function.
    
    - refer to DietCake Notes for more info
    - check parent class, Model (dietcake/core/model.php)
    - validation helpers are found in app/helpers/validation_helper.php

--- app/models/comment.php
+++ app/models/comment.php
@@ -1,4 +1,7 @@
 <?php
 class Comment extends AppModel {
-    
+    public $validation = array(
+        'username'  => array('length' => 'validate_between', 1, 16),
+        'body'      => array('length' => 'validate_between', 1, 200),
+    );
 }
