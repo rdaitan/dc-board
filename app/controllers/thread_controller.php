1510c8f3ee08f255c45048bc8a0309b8365ce202
rdaitan <r.daitan@klab.com>
2015-08-25 15:43:34 +0800

    Wrote ThreadController::write()
    When the user submits to thread/write, write() will be called. The
    thread id is obtained, and the hidden input, page_next is checked
    for its value. If its value is write_end, a new comment instance
    will be created and written to the database. Otherwise, an
    exception is thrown. The view is then rendered.

--- app/controllers/thread_controller.php
+++ app/controllers/thread_controller.php
@@ -14,8 +14,28 @@ class ThreadController extends AppController {
    public function view() {
        $thread = Thread::get(Param::get('thread_id'));
        $comments = $thread->getComments();
-       
+
        $this->set(get_defined_vars());
    }
 
+    public function write() {
+        $thread = Thread::get(Param::get('thread_id'));
+        $page   = Param::get('page_next');
+
+        switch ($page) {
+            case 'write_end':
+                $comment            = new Comment;
+                $comment->username  = Param::get('username');
+                $comment->body      = Param::get('body');
+
+                $thread->write($comment);
+                break;
+            default:
+                throw new NotFoundException("{$page} is not found");
+                break;
+        }
+
+        $this->set(get_defined_vars());
+        $this->render($page);
+    }
 }
