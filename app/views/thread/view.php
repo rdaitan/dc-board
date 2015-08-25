403b0470d2adfdae614637e5530c7707ada70142
rdaitan <r.daitan@klab.com>
2015-08-25 14:51:39 +0800

    Thread Comment Form
    Added a comment form on the thread view. Form contains two hidden
    inputs that are said to be useful later.

--- app/views/thread/view.php
+++ app/views/thread/view.php
@@ -8,3 +8,16 @@
         </div>
     </div>
 <?php } ?>
+
+<!--Comment Form-->
+<hr>
+<form action="<?php eh(url('thread/write')); ?>" class="well" method="post">
+    <label for="">Your Name</label>
+    <input type="text" class="span2" name="username" value="<?php eh(Param::get('username')); ?>">
+    <label for="">Comment</label>
+    <textarea name="body"><?php eh(Param::get('body')); ?></textarea>
+    <br>
+    <input type="hidden" name="thread_id" value="<?php eh($thread->id) ?>">
+    <input type="hidden" name="page_next" value="write_end">
+    <button type="submit" class="btn btn-primary">Submit</button>
+</form>
