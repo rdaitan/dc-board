1a90a08f129ace97ceb020c7f6479f151f1b7734
rdaitan <r.daitan@klab.com>
2015-08-25 17:34:00 +0800

    Corrected validation rules for comment model
    I forgot that the contents of the rule name must be an array of the
    method or function used for validation, and its arguments.

--- app/models/comment.php
+++ app/models/comment.php
@@ -1,7 +1,7 @@
 <?php
 class Comment extends AppModel {
     public $validation = array(
-        'username'  => array('length' => 'validate_between', 1, 16),
-        'body'      => array('length' => 'validate_between', 1, 200),
+        'username'  => array('length' => array('validate_between', 1, 16)),
+        'body'      => array('length' => array('validate_between', 1, 200)),
     );
 }
