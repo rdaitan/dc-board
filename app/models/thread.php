e2c9844196c78fc745e5e944e2665904e3aed092
rdaitan <r.daitan@klab.com>
2015-08-26 14:02:40 +0800

    Adjusted spacing in thread.php
    Some parts of the code use 3 spaces for tabs for some reason.
    Adjusted it so it uses 4.

--- app/models/thread.php
+++ app/models/thread.php
@@ -13,54 +13,54 @@ class Thread extends AppModel {
     }
 
     public static function get($id) {
-       $db = DB::conn();
+        $db = DB::conn();
 
-       $row = $db->row('SELECT * FROM thread WHERE id=?', array($id));
-       if(empty($row))
-           throw new RecordNotFoundException('No record found');
+        $row = $db->row('SELECT * FROM thread WHERE id=?', array($id));
+        if(empty($row))
+            throw new RecordNotFoundException('No record found');
 
-       return new self($row);
-   }
+        return new self($row);
+    }
 
-   public function getComments() {
-       $db = DB::conn();
-       $rows = $db->rows('SELECT * FROM comment WHERE thread_id=? ORDER BY created ASC', array($this->id));
+    public function getComments() {
+        $db = DB::conn();
+        $rows = $db->rows('SELECT * FROM comment WHERE thread_id=? ORDER BY created ASC', array($this->id));
 
-       $comments = array();
-       foreach ($rows as $row) {
-           $comments[] = new Comment($row);
-       }
+        $comments = array();
+        foreach ($rows as $row) {
+            $comments[] = new Comment($row);
+        }
 
-       return $comments;
-   }
+        return $comments;
+    }
 
-   public function write(Comment $comment) {
-       if(!$comment->validate()) {
-           throw new ValidationException('Invalid comment.');
-       }
-       $db = DB::conn();
-       $db->query('INSERT INTO comment SET thread_id=?, username=?, body=?, created=NOW()', array($this->id, $comment->username, $comment->body));
-   }
+    public function write(Comment $comment) {
+        if(!$comment->validate()) {
+            throw new ValidationException('Invalid comment.');
+        }
+        $db = DB::conn();
+        $db->query('INSERT INTO comment SET thread_id=?, username=?, body=?, created=NOW()', array($this->id, $comment->username, $comment->body));
+    }
 
-   public $validation = array(
-       'title' => array('length' => array('validate_between', 1, 30)),
-   );
+    public $validation = array(
+        'title' => array('length' => array('validate_between', 1, 30)),
+    );
 
-   public function create(Comment $comment) {
-       $this->validate(); // before calling this method, $this->title was set first
-       $comment->validate();
-       if($this->hasError() || $comment->hasError()) {
-           throw new ValidationException('Invalid thread or comment.');
-       }
+    public function create(Comment $comment) {
+        $this->validate(); // before calling this method, $this->title was set first
+        $comment->validate();
+        if($this->hasError() || $comment->hasError()) {
+            throw new ValidationException('Invalid thread or comment.');
+        }
 
-       $db = DB::conn();
-       $db->begin();
-       $db->insert('thread', array('title' => $this->title));
+        $db = DB::conn();
+        $db->begin();
+        $db->insert('thread', array('title' => $this->title));
 
-       // write first comment
-       $this->id = $db->lastInsertId();
-       $this->write($comment);
+        // write first comment
+        $this->id = $db->lastInsertId();
+        $this->write($comment);
 
-       $db->commit();
-   }
+        $db->commit();
+    }
 }
