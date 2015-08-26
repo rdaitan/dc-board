5efdae7251b0ab4da760934929b6e9b30fd09df9
rdaitan <r.daitan@klab.com>
2015-08-26 11:04:22 +0800

    Wrote thread/create action
    Shows a form to create a new thread. Writes the new thread to the
    database. Redirect to create_end page if successful, show form if
    not.
    
    - view not yet available

--- app/controllers/thread_controller.php
+++ app/controllers/thread_controller.php
@@ -43,4 +43,34 @@ class ThreadController extends AppController {
         $this->set(get_defined_vars());
         $this->render($page);
     }
+
+    public function create() {
+        $page = Param::get('page_next', 'create');
+
+        switch($page) {
+            case 'create':
+                break;
+            case 'create_end':
+                $thread             = new Thread;
+                $thread->title      = Param::get('title');
+
+                $comment            = new Comment;
+                $comment->username  = Param::get('username');
+                $comment->body      = Param::get('body');
+
+                try {
+                    $thread->create($comment);
+                }
+                catch(ValidationException $e) {
+                    $page = 'create';
+                }
+                break;
+            default:
+                throw new NotFoundException("{$page} is not found");
+                break;
+        }
+
+        $this->set(get_defined_vars());
+        $this->render($page);
+    }
 }
