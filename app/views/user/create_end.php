684d99d2962dd10f0b3b5d3e8f1e44e3dd2681fc
rdaitan <r.daitan@klab.com>
2015-08-27 16:13:31 +0800

    Show login form after authentication
    This is temporary. For testing only.

--- app/views/user/create_end.php
+++ app/views/user/create_end.php
@@ -1,1 +1,12 @@
 <h2>Success! TODO: authentication</h2>
+
+<!--Login form-->
+<form action="<?php eh(url('user/authenticate')); ?>" method="post">
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username">
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password">
+    <br>
+    <input type="hidden" name="page_next" value="auth_end">  
+    <button type="submit">Log In</button>
+</form>
