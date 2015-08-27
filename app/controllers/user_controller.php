87f4f53d000f8c75cdac616a9ebef65e979cb83d
rdaitan <r.daitan@klab.com>
2015-08-27 13:14:59 +0800

    Implemented UserController::create()
    Tries to insert the new user to the table. If failed, render
    the registration page again, with the error messages.

--- app/controllers/user_controller.php
+++ app/controllers/user_controller.php
@@ -2,7 +2,27 @@
 class UserController extends AppController
 {
     public function create() {
-        // For now, just render the registration form in
-        // views/user/create.php
+        $user = new User;
+        $page = Param::get('page_next');
+
+        switch ($page) {
+            case 'create_end':
+                $user->username = trim(Param::get('username'));
+                $user->email    = trim(Param::get('email'));
+                $user->password = Param::get('password'); // TODO: hash
+
+                try {
+                    $user->create();
+                } catch(ValidationException $e) {
+                    $page = 'create';
+                }
+                break;
+            default:
+                throw new NotFoundException("{$page} is not found.");
+                break;
+        }
+
+        $this->set(get_defined_vars());
+        $this->render($page);
     }
 }
