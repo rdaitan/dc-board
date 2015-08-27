3499494e9d9ce71db1b4add9acff5f18cf249413
rdaitan <r.daitan@klab.com>
2015-08-27 16:49:00 +0800

    Added $salt parameter in hash_password()
    The $salt parameter is an optional parameter in the hash_password()
    function. If specified, it would use the specified salt instead of
    generating a new one. This way, it can be used on situations like
    authentication.

--- app/helpers/authentication_helper.php
+++ app/helpers/authentication_helper.php
@@ -7,13 +7,14 @@ define('CRYPT_BFISH', '$2a$11$');
 // returns the hashed password, using blowfish alorithm.
 // blowfish algorithm is recommended by the php manual:
 // http://php.net/manual/en/faq.passwords.php
-function hash_password($password)
+function hash_password($password, $salt = null)
 {
-
-    // the salt is only 22 characters long.
-    // openssl_random_pseudo_bytes() would generate 88 bits.
-    // bin2hex() would produce 22 hex characters (88/4).
-    $salt = bin2hex(openssl_random_pseudo_bytes(11));
+    if(empty($salt)) {
+        // the salt is only 22 characters long.
+        // openssl_random_pseudo_bytes() would generate 88 bits.
+        // bin2hex() would produce 22 hex characters (88/4).
+        $salt = bin2hex(openssl_random_pseudo_bytes(11));
+    }
 
     return crypt($password, CRYPT_BFISH . $salt);
 }
