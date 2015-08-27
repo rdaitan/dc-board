b29dd09f62d56c93b552bd0db00c4e558e48b946
rdaitan <r.daitan@klab.com>
2015-08-27 11:47:55 +0800

    Created UserController and user/create view
    UserController has one method, create(), whose purpose for now, is
    to render the user/create view. user/create.php shows a form for
    registering new users.

---
+++ app/controllers/user_controller.php
@@ -0,0 +1,8 @@
+<?php
+class UserController extends AppController
+{
+    public function create() {
+        // For now, just render the registration form in
+        // views/user/create.php
+    }
+}
---
+++ app/views/user/create.php
@@ -0,0 +1,13 @@
+<h2>Register</h2>
+
+<form action="" method="post">
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username">
+    <label for="email">Email</label>
+    <input type="email" name="" id="email">
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password">
+    <br>
+    <button type="submit">Register</button> or
+    <a href="">cancel</a>
+</form>
