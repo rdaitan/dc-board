6432744cbff9dd9c0cf0af18c99d5fc3f0829fb3
rdaitan <r.daitan@klab.com>
2015-08-28 14:40:32 +0800

    Added login and signup buttons in index
    They show up when you're not logged in, and hide when you are.

--- app/views/thread/index.php
+++ app/views/thread/index.php
@@ -10,3 +10,7 @@
     <?php } ?>
     <a href="<?php eh(url('thread/create')); ?>" class="btn btn-large btn-primary">Create</a>
 </ul>
+<?php if(!User::getAuthUser()) { ?>
+    <a href="<?php eh(url('user/create')); ?>" class="btn btn-primary">Sign Up</a>
+    <a href="<?php eh(url('user/authenticate')); ?>" class="btn btn-success">Log in</a>
+<?php } ?>
