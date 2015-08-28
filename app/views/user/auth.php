00821b5aaba8dd5675825e7f8765cfbd5d7a1325
rdaitan <r.daitan@klab.com>
2015-08-28 08:56:55 +0800

    Set previous input values as default
    In the log in screen, the previous input values will be set if
    authentication fails.

--- app/views/user/auth.php
+++ app/views/user/auth.php
@@ -7,9 +7,9 @@
 <?php } ?>
 <form action="<?php eh(url('user/authenticate')); ?>" method="post">
     <label for="username">Username</label>
-    <input type="text" name="username" id="username">
+    <input type="text" name="username" id="username" value='<?php eh(Param::get('username')); ?>'>
     <label for="password">Password</label>
-    <input type="password" name="password" id="password">
+    <input type="password" name="password" id="password" value='<?php eh(Param::get('password')); ?>'>
     <br>
     <input type="hidden" name="page_next" value="auth_end">
     <button type="submit">Log in</button> or <a href="<?php eh(url('/')) ?>">cancel</a>
