da7ef7f8ccc00f2f5ebb5d638234ce671e81dcbb
rdaitan <r.daitan@klab.com>
2015-08-28 09:25:30 +0800

    Made bhash() more readable

--- app/helpers/authentication_helper.php
+++ app/helpers/authentication_helper.php
@@ -1,5 +1,5 @@
 <?php
-# Authentication helpers based on:
+# Authentication helper based on:
 # http://snippetrepo.com/snippets/php-53-bcrypt-password-hash-the-proper-way
 
 define('CRYPT_BFISH', '$2a$11$');
@@ -9,11 +9,15 @@ define('CRYPT_BFISH', '$2a$11$');
 // http://php.net/manual/en/faq.string.php
 function bhash($str, $salt = null)
 {
+
     if(empty($salt)) {
-        // the salt is only 22 characters long.
-        // openssl_random_pseudo_bytes() would generate 88 bits.
-        // bin2hex() would produce 22 hex characters (88/4).
-        $salt = bin2hex(openssl_random_pseudo_bytes(11));
+        // the salt length for blowfish is only 22 characters long.
+        // bin2hex() would produce 22 hex characters
+        // 22 characters * 4 bits = 88 bits
+        // openssl_random_pseudo_bytes() needs the number of bytes to generate.
+        // 88 bits / 8 bits = 11 bytes
+        $bitCount   = 11;
+        $salt       = bin2hex(openssl_random_pseudo_bytes($bitCount));
     }
 
     return crypt($str, CRYPT_BFISH . $salt);
