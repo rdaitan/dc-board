44f0cc011d530dfaca41952b33b17b89d2310713
rdaitan <r.daitan@klab.com>
2015-09-01 12:01:46 +0800

    Styled thread create_end page

--- app/views/thread/create_end.php
+++ app/views/thread/create_end.php
@@ -1,6 +1,8 @@
-<h2><?php eh($thread->title); ?></h2>
-
-<p class="alert alert-success">You successfully created a thread.</p>
-<a href="<?php eh(url('thread/view', array('thread_id' => $thread->id))); ?>">
-    &larr; Go to thread
-</a>
+<div class="row">
+    <div class="col-md-8 col-md-offset-2">
+        <div class="alert alert-success">
+            <h3>Thread created!</h3>
+            <a href="<?php eh(url('thread/view', array('thread_id' => $thread->id))) ?>">View thread &rarr;</a>
+        </div>
+    </div>
+</div>
