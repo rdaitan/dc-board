2c2ea468f34ce7be9ba7e2d95adcdb5d751f3c7d
rdaitan <r.daitan@klab.com>
2015-09-01 12:17:08 +0800

    Styled thread view comment form
    Copy-pasted from comment/create.php

--- app/views/thread/view.php
+++ app/views/thread/view.php
@@ -15,13 +15,26 @@
 <!--Comment Form-->
 <?php if(User::getAuthUser()) {?>
     <hr>
-    <form action="<?php eh(url('comment/create')); ?>" class="well" method="post">
-        <label for="">Comment</label>
-        <textarea name="body"><?php eh(Param::get('body')); ?></textarea>
-        <br>
-        <input type="hidden" name="thread_id" value="<?php eh($thread->id) ?>">
-        <input type="hidden" name="page_next" value="create_end">
-        <button type="submit" class="btn btn-primary">Submit</button>
+    <form class='form-horizontal' action="<?php eh(url('comment/create')); ?>" class="well" method="post">
+        <div class="form-group">
+            <div class="col-sm-12">
+                <h3>Create comment</h3>
+            </div>
+        </div>
+        <div class="form-group">
+            <label class='col-sm-2 control-label' for="body">Comment</label>
+            <div class="col-sm-10">
+                <textarea class='form-control' id='body' name="body"><?php eh(Param::get('body')); ?></textarea>
+            </div>
+        </div>
+        <div class="form-group">
+            <input type="hidden" name="thread_id" value="<?php eh($thread->id) ?>">
+            <input type="hidden" name="page_next" value="create_end">
+            <div class="col-sm-10 col-sm-offset-2">
+                <button type="submit" class="btn btn-primary">Submit</button> or
+                <a href="<?php eh(url('thread/view', array('thread_id' => $thread->id))); ?>">cancel</a>
+            </div>
+        </div>
     </form>
 <?php } else {?>
     <a href="<?php eh(url('user/authenticate')) ?>" class="btn btn-primary">Comment</a>
