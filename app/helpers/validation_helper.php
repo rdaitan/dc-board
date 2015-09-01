78d84c1886013d5749543c9261bc3160dbd94492
rdaitan <r.daitan@klab.com>
2015-09-01 16:18:41 +0800

    Formatting changes. Removed unused function.
    validate_name() was used to check the validity of the user's real
    name. Now that the real name is not required during registration,
    it is no longer needed.

--- app/helpers/validation_helper.php
+++ app/helpers/validation_helper.php
@@ -1,21 +1,21 @@
 <?php
-function validate_between($str, $min, $max) {
+function validate_between($str, $min, $max)
+{
     $length = mb_strlen($str); // count characters based on encoding
     return $length <= $max && $length >= $min;
 }
 
-function validate_name($str) {
-    return preg_match('/^[[:alpha:] ]*$/', $str);
-}
-
-function validate_username($str) {
+function validate_username($str)
+{
     return preg_match('/^[[:alnum:]_]*$/', $str);
 }
 
-function validate_unique_name($username) {
+function validate_unique_name($username)
+{
     return !User::getByUsername($username);
 }
 
-function validate_email($str) {
+function validate_email($str)
+{
     return filter_var($str, FILTER_VALIDATE_EMAIL);
 }
