821ebb1ee3a9b10c0a17a6853c0403bdbaf56027
rdaitan <r.daitan@klab.com>
2015-09-01 12:14:14 +0800

    Styled comment create form
    Removed username field. Added a cancel link.

--- app/views/comment/create.php
+++ app/views/comment/create.php
@@ -1,41 +1,49 @@
-<h2><?php eh($thread->title); ?></h2>
-
-<?php if($comment->hasError()) { ?>
-    <div class="alert alert-block">
-        <h4 class="alert-heading">Validation error!</h4>
-
-        <?php if(!empty($comment->validation_errors['username']['length'])) { ?>
-            <div>
-                <em>Username</em> must be between
-                <?php eh($comment->validation['username']['length'][1]); ?> and
-                <?php eh($comment->validation['username']['length'][2]); ?> characters
-                in length.
-            </div>
-        <?php } ?>
-        <?php if(!empty($comment->validation_errors['username']['char'])) { ?>
-            <div>
-                <em>Your name</em> must only have letters and spaces.
-            </div>
-        <?php } ?>        
-        <?php if(!empty($comment->validation_errors['body']['length'])) { ?>
-            <div>
-                <em>Comment</em> must be between
-                <?php eh($comment->validation['body']['length'][1]); ?> and
-                <?php eh($comment->validation['body']['length'][2]); ?> characters
-                in length.
-            </div>
-        <?php } ?>
+<div class="row">
+    <div class="col-md-12">
+        <h2><?php eh($thread->title); ?></h2>
     </div>
-<?php } ?>
+    <div class="col-md-6 col-md-offset-3">
+        <?php if($comment->hasError()) { ?>
+            <div class="alert alert-danger">
+                <h4 class="alert-heading">Validation error!</h4>
 
-<!--Comment form-->
-<form action="<?php eh(url('comment/create')); ?>" class="well" method="post">
-    <label for="">Your Name</label>
-    <input type="text" class="span2" name="username" value="<?php eh(Param::get('username')); ?>">
-    <label for="">Comment</label>
-    <textarea name="body"><?php eh(Param::get('body')); ?></textarea>
-    <br>
-    <input type="hidden" name="thread_id" value="<?php eh($thread->id) ?>">
-    <input type="hidden" name="page_next" value="create_end">
-    <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+                <?php if(!empty($comment->validation_errors['username']['char'])) { ?>
+                    <div>
+                        <em>Your name</em> must only have letters and spaces.
+                    </div>
+                <?php } ?>
+                <?php if(!empty($comment->validation_errors['body']['length'])) { ?>
+                    <div>
+                        <em>Comment</em> must be between
+                        <?php eh($comment->validation['body']['length'][1]); ?> and
+                        <?php eh($comment->validation['body']['length'][2]); ?> characters
+                        in length.
+                    </div>
+                <?php } ?>
+            </div>
+        <?php } ?>
+
+        <!--Comment form-->
+        <form class='form-horizontal' action="<?php eh(url('comment/create')); ?>" class="well" method="post">
+            <div class="form-group">
+                <div class="col-sm-12">
+                    <h3>Create comment</h3>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class='col-sm-2 control-label' for="body">Comment</label>
+                <div class="col-sm-10">
+                    <textarea class='form-control' id='body' name="body"><?php eh(Param::get('body')); ?></textarea>
+                </div>
+            </div>
+            <div class="form-group">
+                <input type="hidden" name="thread_id" value="<?php eh($thread->id) ?>">
+                <input type="hidden" name="page_next" value="create_end">
+                <div class="col-sm-10 col-sm-offset-2">
+                    <button type="submit" class="btn btn-primary">Submit</button> or
+                    <a href="<?php eh(url('thread/view', array('thread_id' => $thread->id))); ?>">cancel</a>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
