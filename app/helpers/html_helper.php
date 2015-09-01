576aa11000ad6767c5445efdee0fa963cd663010
rdaitan <r.daitan@klab.com>
2015-09-01 16:16:53 +0800

    Readability changes

--- app/helpers/html_helper.php
+++ app/helpers/html_helper.php
@@ -45,6 +45,7 @@ function printPageLinks($pagination, $pages)
 
     echo '<nav><ul class="pagination">';
 
+    // previous button
     if($pagination->current > 1) {
         $url = url('', array('page' => $pagination->prev));
         echo "<li><a href='{$url}'>&laquo;</a></li>";
@@ -52,6 +53,7 @@ function printPageLinks($pagination, $pages)
         echo "<li class='disabled'><a>&laquo;</a></li>";
     }
 
+    // page numbers
     for($i = 1; $i <= $pages; $i++) {
         if($i == $page) {
             echo "<li class='disabled'><a>{$i}</a></li>";
@@ -61,11 +63,12 @@ function printPageLinks($pagination, $pages)
         }
     }
 
-    if(!$pagination->is_last_page) {
+    // next button
+    if($pagination->is_last_page) {
+        echo "<li class='disabled'><a>&raquo;</a></li>";
+    } else {
         $url = url('', array('page' => $pagination->next));
         echo "<li><a href='{$url}'>&raquo;</a></li>";
-    } else {
-        echo "<li class='disabled'><a>&raquo;</a></li>";
     }
 
     echo '</ul></nav>';
