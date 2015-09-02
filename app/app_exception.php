f88b3f261b3a3d588d8c30c18323d1720d1fb28d
rdaitan <r.daitan@klab.com>
2015-09-02 08:03:41 +0800

    Formatting
    Please follow this format po :
    
    class AppException extends Exception
    {
    }

--- app/app_exception.php
+++ app/app_exception.php
@@ -1,4 +1,12 @@
 <?php
-class AppException extends Exception {}
-class ValidationException extends Exception {}
-class RecordNotFoundException extends AppException {};
+class AppException extends Exception
+{
+}
+
+class ValidationException extends Exception
+{
+}
+
+class RecordNotFoundException extends AppException
+{
+}
