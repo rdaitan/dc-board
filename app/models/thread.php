dd367eb175a30bb83f303ba6ea30ad390d624712
rdaitan <r.daitan@klab.com>
2015-09-02 14:43:43 +0800

    Revert "Spacing"
    This reverts commit 3270b3b1877ac1d5e0cd19f1c27f9251cafe701b.

--- app/models/thread.php
+++ app/models/thread.php
@@ -23,8 +23,7 @@ class Thread extends AppModel
         $db = DB::conn();
 
         $row = $db->row('SELECT * FROM thread WHERE id=?', array($id));
-
-        if(!$row) {
+        if(!$row){
             throw new RecordNotFoundException('No record found');
         }
 
@@ -50,7 +49,7 @@ class Thread extends AppModel
             $comment->create($this);
 
             $db->commit();
-        } catch(PDOException $e) {
+        } catch (PDOException $e) {
             $db->rollback();
         }
     }
