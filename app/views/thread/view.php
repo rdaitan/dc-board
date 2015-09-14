6a8a4351bf41c3af8695fcfdd55103a24d96c872
rdaitan <r.daitan@klab.com>
2015-09-14 16:39:21 +0800

    Added follow and unfollow links in thread view
    Follow shows up when user is not following a thread, and unfollow
    shows up when usser is already following it.
    
    Added text in follow/unfollow link
    
    Defined url constants
    
    For follow.
    
    Fixed follow links

--- app/views/thread/view.php
+++ app/views/thread/view.php
@@ -7,6 +7,11 @@
                 <a href='<?php eh(url(EDIT_THREAD_URL, array('id' => $thread->id))); ?>'>Edit thread</a>
                 <a href='<?php eh(url(DELETE_THREAD_URL, array('id' => $thread->id))); ?>'>Delete thread</a>
             <?php endif; ?>
+            <?php if ($thread->isFollowedBy($auth_user)): ?>
+                <a href="<?php eh(url(FOLLOW_URL, array('id' => $thread->id))); ?>">Unfollow</a>
+            <?php else: ?>
+                <a href="<?php eh(url(FOLLOW_URL, array('id' => $thread->id))); ?>">Follow</a>
+            <?php endif; ?>
         </div>
         <div class="offset-top">
             <!--comment-->
