579919010eb636cb16178e0235e2bac04c26d2ba
rdaitan <r.daitan@klab.com>
2015-09-14 14:31:20 +0800

    Updated view to display comment count

--- app/views/thread/rank.php
+++ app/views/thread/rank.php
@@ -1,6 +1,9 @@
 <h2>Trending</h2>
 <ul>
     <?php foreach ($threads as $thread): ?>
-        <li><a href="<?php eh(url(VIEW_THREAD_URL, array('id' => $thread->id))); ?>"><?php eh($thread->title); ?></a></li>
+        <li>
+            <a href="<?php eh(url(VIEW_THREAD_URL, array('id' => $thread->id))); ?>"><?php eh($thread->title); ?></a>
+            <strong><?php eh($thread->count); ?></strong>
+        </li>
     <?php endforeach; ?>
 </ul>
