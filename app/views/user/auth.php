fccc8608dc0f00baf944f68ae20a30f8ab372298
rdaitan <r.daitan@klab.com>
2015-09-17 07:44:16 +0800

    finished log in

--- app/views/user/auth.php
+++ app/views/user/auth.php
@@ -9,32 +9,21 @@
     <?php endif; ?>
 </div>
 <div class="row">
-    <div class="col-md-6 col-md-offset-3">
-        <form class='form-horizontal' action="<?php eh(url(LOGIN_URL)); ?>" method="post">
-            <div class="form-group">
-                <div class="col-sm-12">
-                    <h3>Login</h3>
+    <form action="<?php eh(url(LOGIN_URL)); ?>" method="post">
+            <div class="row">
+                <div class="six columns">
+                    <label for="username">Username</label>
+                    <input class='u-full-width'type="text" name="username" id="username" value='<?php eh(Param::get('username')); ?>'>
+                </div>
+                <div class="six columns">
+                    <label for="password">Password</label>
+                    <input class='u-full-width'type="password" name="password" id="password">
                 </div>
             </div>
-            <div class="form-group">
-                <label class='control-label col-sm-2' for="username">Username</label>
-                <div class="col-sm-10">
-                    <input class='form-control'type="text" name="username" id="username" value='<?php eh(Param::get('username')); ?>'>
-                </div>
-            </div>
-            <div class="form-group">
-                <label class='control-label col-sm-2' for="password">Password</label>
-                <div class="col-sm-10">
-                    <input class='form-control'type="password" name="password" id="password" value='<?php eh(Param::get('password')); ?>'>
-                </div>
-            </div>
-            <div class="form-groupl">
+            <div class="row">
                 <input type="hidden" name="page_next" value="auth_end">
-                <div class="col-sm-10 col-sm-offset-2">
-                    <button class='btn btn-primary' type="submit">Log in</button> or
-                    <a href="<?php eh(url(APP_URL)) ?>">cancel</a>
-                </div>
+                <button type="submit">Log in</button> or
+                <a href="<?php eh(url(APP_URL)) ?>">cancel</a>
             </div>
-        </form>
-    </div>
+    </form>
 </div>
