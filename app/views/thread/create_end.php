491aa4b47200bfea03a6ee7795d3f66b788ab64d
rdaitan <r.daitan@klab.com>
2015-09-17 10:28:44 +0800

    Removed unused views

--- app/views/thread/create_end.php
+++
@@ -1,8 +0,0 @@
-<div class="row">
-    <div class="col-md-8 col-md-offset-2">
-        <div class="alert alert-success">
-            <h3>Thread created!</h3>
-            <a href="<?php eh(url(VIEW_THREAD_URL, array('id' => $thread->id))) ?>">View thread &rarr;</a>
-        </div>
-    </div>
-</div>
